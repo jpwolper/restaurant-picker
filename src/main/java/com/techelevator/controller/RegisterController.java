8326998aca3d05d07bccdddbebe54c6bd8330dfb
Ethan Ginty <ethan.ginty@yahoo.com>
2017-12-22 12:56:12 -0500

    controller update

--- src/main/java/com/techelevator/controller/RegisterController.java
+++ src/main/java/com/techelevator/controller/RegisterController.java
@@ -2,12 +2,16 @@ package com.techelevator.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.techelevator.dao.RestaurantPickerDAO;
+import com.techelevator.model.User;
 
 @Controller
 public class RegisterController {
@@ -25,7 +29,15 @@ public class RegisterController {
 	}
 
 	@RequestMapping(path = "/register", method = RequestMethod.POST)
-	public String postRegisterPage(Map<String, Object> model) {
+	public String postRegisterPage(ModelMap model, HttpServletRequest request) {
+		User newUser = new User();
+		newUser.setFirstName(request.getParameter("firstName"));
+		newUser.setLastName(request.getParameter("lastName"));
+		newUser.setPassword(request.getParameter("password"));
+		newUser.setUserId(Integer.parseInt(request.getParameter("userId")));
+		newUser.setSalt(request.getParameter("salt"));
+		newUser.setUserName(request.getParameter("userName"));
+		model.put("user", rpDAO.saveUser(newUser));
 		return "register";
 	}
 }
