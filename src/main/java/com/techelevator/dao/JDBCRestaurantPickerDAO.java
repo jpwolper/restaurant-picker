48403f0b9036f0b7e396c60e1ed92ed8f8835ea6
Ross Fischer <rossgfischer@gmail.com>
2017-12-22 15:46:42 -0500

    JDBC added

--- src/main/java/com/techelevator/dao/JDBCRestaurantPickerDAO.java
+++ src/main/java/com/techelevator/dao/JDBCRestaurantPickerDAO.java
@@ -1,5 +1,6 @@
 package com.techelevator.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
@@ -7,8 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
-
-
 import com.techelevator.model.Restaurant;
 import com.techelevator.model.User;
 
@@ -39,14 +38,28 @@ public class JDBCRestaurantPickerDAO implements RestaurantPickerDAO {
 
 	@Override
 	public List<Restaurant> getRestaurants(String foodType, int minRating) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Restaurant> newRestaurants = new ArrayList<Restaurant>();
+		String sqlGetRestaurant = "Select * FROM restaurants WHERE foodtype = ? AND rating = ?";
+		SqlRowSet results = jdbctemplate.queryForRowSet(sqlGetRestaurant, foodType, minRating);
+		while (results.next()) {
+			newRestaurants.add(mapRowToRestaurant(results));
+			results.next();
+		}	
+		
+		return newRestaurants;
 	}
 
 	@Override
 	public List<String> getFoodTypes() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> allFoodTypes = new ArrayList<String>();
+		String sqlGetAllFoodTypes = "Select DISTINCT foodtype FROM restaurant";
+		SqlRowSet results = jdbctemplate.queryForRowSet(sqlGetAllFoodTypes);
+		while (results.next()) {
+			allFoodTypes.add(results.getString("foodtype"));
+			results.next();
+		}	
+		
+		return allFoodTypes;
 	}
 
 	private User mapRowToUser(SqlRowSet results) {
@@ -58,5 +71,13 @@ public class JDBCRestaurantPickerDAO implements RestaurantPickerDAO {
 		u.setLastName(results.getString("lastname"));
 		return u;
 	}
-	
+	private Restaurant mapRowToRestaurant(SqlRowSet results) {
+		Restaurant rest = new Restaurant();
+		rest.setRestaurantId(results.getLong("restaurantid"));
+		rest.setStreetAddress(results.getString("streetAddress"));
+		rest.setZipCode(results.getString("zipcode"));
+		rest.setFoodType(results.getString("foodtype"));
+		rest.setRating(results.getInt("rating"));
+		return rest;
+	}
 }
